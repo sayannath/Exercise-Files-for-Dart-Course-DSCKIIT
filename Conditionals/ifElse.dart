9b2ba85ee3952fb06af64c7a7460e5d762094307
sayannath <sayannath235@gmail.com>
2020-04-06 14:29:19 +0530

    Conditionals

---
+++ Conditionals/ifElse.dart
@@ -0,0 +1,22 @@
+main() {
+
+  //Initialisation
+  // var a = 10;
+  // var b = 20;
+
+  // //Checking
+  // if(b > a)  // 20 > 10 (True)
+  // print("B is greater than A.");
+  // else
+  // print("A is greater than B.");
+
+  var temp = 35;
+
+  if(temp >=40) {
+    print("Stay Home.");
+    print("If you are going out, take a umbrella.");
+  }
+  else {
+    print("Go to Work.");
+  }
+}
