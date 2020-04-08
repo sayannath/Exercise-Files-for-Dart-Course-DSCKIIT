03ab550449cb0f25eeb37bb5021c0c8371918e29
sayannath <sayannath235@gmail.com>
2020-04-08 17:54:56 +0530

    Conditionals

--- Conditionals/switchCase.dart
+++ Conditionals/switchCase.dart
@@ -1,22 +1,18 @@
 main() {
-
   var enterChoice = "2";
 
-  switch(enterChoice) {
-
+  switch (enterChoice) {
     case "1":
-    print("User 1");
-    break;
+      print("User 1");
+      break;
     case "2":
-    print("User 2");
-    break;
+      print("User 2");
+      break;
     case "3":
-    print("user 3");
-    break;
+      print("user 3");
+      break;
 
     default:
-    print("Wrong choice is entered");
-  
+      print("Wrong choice is entered");
   }
-
 }
