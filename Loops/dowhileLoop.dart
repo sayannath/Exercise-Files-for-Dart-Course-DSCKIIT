e7ce3520a239695afe9f0f872dbaa28c5bdeb699
sayannath <sayannath235@gmail.com>
2020-04-16 15:26:23 +0530

    Loops

---
+++ Loops/dowhileLoop.dart
@@ -0,0 +1,14 @@
+main() {
+  //Loop = to repeat same steps multiple times
+
+  var i = 11; // Initialisation part
+  do {
+  
+    print("hello $i");
+    i = i + 1; //Incrementing/Decrementing part
+    //i++;
+  } while (i <= 5);  //Condition Part 
+  print("Done with the loop.");
+}
+
+// TODO: Write a program to print all the even number from 1 to 50
---
+++ Loops/whileLoop.dart
@@ -0,0 +1,16 @@
+main() {
+
+  //Loop = to repeat same steps multiple times
+
+  var i = 11; // Initialisation part
+
+  while (i<=5) { //Condition Part // 6<=5(F)
+    
+    print("hello $i");
+    i=i+1; //Incrementing/Decrementing part
+    //i++;
+  }
+  print("Done with the loop.");
+
+}
+
