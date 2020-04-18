772c888f472e2ccc445c19b41129e24cadc04275
sayannath <sayannath235@gmail.com>
2020-04-18 17:53:40 +0530

    Loops

---
+++ Loops/forLoop.dart
@@ -0,0 +1,16 @@
+// TODO: Write a program to print all the even number from 1 to 20
+
+// main() {
+//   for (var i = 1; i <= 20; i++) {
+//     if (i % 2 == 0) print(i);
+//   }
+// }
+
+main() {
+
+List courseList = ["Dart", "Java", "Python", "C++", "Golang"];
+
+  for(var courses in courseList) 
+  print(courses);
+
+}
