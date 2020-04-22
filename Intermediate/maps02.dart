604678186a56113ae15190629a47e4f9acc800e4
sayannath <sayannath235@gmail.com>
2020-04-22 16:21:13 +0530

    Maps

---
+++ Intermediate/maps02.dart
@@ -0,0 +1,17 @@
+main() {
+
+  var user = new Map();
+
+  user["PIN"] = "700091";
+  user["City"] = "Kolkata";
+
+  // user.addAll({
+  //   "username": "Sayan Nath",
+  //   "email": "sayannath235@gmail.com",
+  //   "password": "password",
+  //   "college": "KIIT"
+  // });
+
+  print(user);
+
+}
