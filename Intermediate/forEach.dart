efac1a2aaa6f2f5a298c2a449a622f56db7c12cd
sayannath <sayannath235@gmail.com>
2020-04-24 14:22:48 +0530

    For Each

---
+++ Intermediate/forEach.dart
@@ -0,0 +1,10 @@
+main() {
+  var user = {
+    "firstName": "Sayan",
+    "lastName": "Nath",
+    "email": "sayannath235@gmail.com",
+    "Instagram id": "sayannath235"
+  };
+
+  user.forEach((k, v) => print("Key is ${k} and Value is ${v}."));
+}
