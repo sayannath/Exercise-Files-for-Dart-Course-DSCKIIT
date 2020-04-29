16839ba89f6492d3dbbf4b33b0f7328779662e87
sayannath <sayannath235@gmail.com>
2020-04-29 17:28:38 +0530

    Objects

--- OOPS/demo.dart
+++ OOPS/demo.dart
@@ -1,5 +1,4 @@
 class Course {
-
   //fields/properties
   int id;
   String name, price;
@@ -10,7 +9,20 @@ class Course {
 
   //Functions
   void courseIsLive() {
-    print("${this.name} is now live.");
+    print("${this.name} is now live at a price of ${price}.");
   }
+}
+
+main() {
+
+  var courseInfo = Course();
+  courseInfo.name = "Dart";
+  courseInfo.price = "199";
+  courseInfo.courseIsLive();
+
+  var javaInfo = Course();
+  javaInfo.name = "Java";
+  javaInfo.price = "0";
+  javaInfo.courseIsLive();
 
 }
