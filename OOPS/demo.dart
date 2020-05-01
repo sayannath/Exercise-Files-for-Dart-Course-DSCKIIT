5ec2e11471e86ee97b2c37d21fa7ed555e2deb8f
sayannath <sayannath235@gmail.com>
2020-05-01 13:45:11 +0530

    Contructors

--- OOPS/demo.dart
+++ OOPS/demo.dart
@@ -6,6 +6,10 @@ class Course {
   //Getters and Setters
 
   // Constructor
+  Course(String name, String price) {
+    this.name = name;
+    this.price = price;
+  }
 
   //Functions
   void courseIsLive() {
@@ -15,14 +19,14 @@ class Course {
 
 main() {
 
-  var courseInfo = Course();
-  courseInfo.name = "Dart";
-  courseInfo.price = "199";
+  var courseInfo = new Course("Dart", "199");
+  // courseInfo.name = "Dart";
+  // courseInfo.price = "199";
   courseInfo.courseIsLive();
 
-  var javaInfo = Course();
-  javaInfo.name = "Java";
-  javaInfo.price = "0";
-  javaInfo.courseIsLive();
+  var javaInfo = new Course("Java", "299");
+  // javaInfo.name = "Java";
+  // javaInfo.price = "0";
+   javaInfo.courseIsLive();
 
 }
