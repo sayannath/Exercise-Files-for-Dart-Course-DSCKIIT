ccaca04241e4275ae8593b5fbf91bc414c8a5159
sayannath <sayannath235@gmail.com>
2020-05-01 13:01:50 +0530

    Add

---
+++ Basics/variableDatatypes.dart
@@ -0,0 +1,21 @@
+void main() {
+  var x = 235; // int x = 235;
+  var PI = 3.14;
+  double d = 56.90;
+
+  //print("x+PI+d");
+
+  var name = "Sayan Nath";
+  var age = "19"; // var age = 19; are totally different.
+
+  //print("Name is "+name+" and the age is "+age+".");
+
+  var isVerified = true;
+  //print(isVerified);
+
+  var isEmptyMy = "Something is here.";
+  print(isEmptyMy.isEmpty);
+
+}
+
+// + - * / %
